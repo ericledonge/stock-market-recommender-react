682253c38d5487b73ab79b7e179002533abfb924
Eric <eric.ledonge@gmail.com>
2020-09-19 11:27:30 -0400

    :lipstick: Header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__title">BSMR</div>
+      <div className="header__subtitle">The First Stock Market Recommender Based On Behavioral Financal AI.
+      </div>
+    </div>
+  );
+};
+
+export default Header;
