dc08bb00e30eb572a12edddc37b5e11ab1afbeaa
Eric <eric@kumojin.com>
2020-09-19 20:52:28 -0400

    WIP Before Moment

---
+++ src/api/mocks/mocks.js
@@ -0,0 +1,5 @@
+export const stockPriceGenerator = (stockSymbol, dates) => {
+
+};
+
+
---
+++ src/api/mocks/mocks.spec.js
@@ -0,0 +1,19 @@
+import { stockPriceGenerator } from './mocks';
+
+describe('stockPriceGenerator', () => {
+  const STOCK_SYMBOL = 'GOOG';
+  const
+
+    describe;
+  ('when duration is 1 day', () => {
+    it('returns a table with an object with date is today and value is random between 100 and 200', () => {
+      const stockPrices = stockPriceGenerator(STOCK_SYMBOL, 1);
+      const expected = [
+        { date: }
+      ];
+      expect(stockPrices).toEqual();
+    });
+  });
+
+
+});
