01c9874f7a6922051ded4183372921c14fb5722b
Eric <eric.ledonge@gmail.com>
2020-09-20 16:58:10 -0400

    :sparkles: Graphs

--- src/components/results/stock-prices/StockPrices.js
+++ src/components/results/stock-prices/StockPrices.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import appContext from '../../../context/appContext';
+import { Line } from 'react-chartjs-2';
 
 const StockPrices = () => {
   const {
     state: { stockPrices },
   } = appContext.useApp();
 
+  const dataMapped = {
+    labels: stockPrices.map(el => el.date),
+    datasets: [
+      {
+        label: 'price (USD)',
+        data: stockPrices.map(el => el.price),
+        fill: true,
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        borderColor: 'rgba(75,192,192,1)'
+      }
+    ]
+  };
+
   return (
     <div className="stock-prices">
-      <table>
-        <thead>
-        <tr>
-          <th>Day</th>
-          <th>Price</th>
-        </tr>
-        </thead>
-        <tbody>
-        {stockPrices.map((day, index) => (
-          <tr key={index}>
-            <td>{day.date}</td>
-            <td>{day.price}</td>
-          </tr>
-        ))}
-        </tbody>
-      </table>
+      <div className="stock-prices__graph">
+        <Line
+          data={dataMapped}
+          options={{}}
+          width={400}
+          height={400}
+        />
+      </div>
     </div>
   );
 };
