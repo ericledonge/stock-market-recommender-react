2e2344f0369d940a8d0de8e75d8523c6611ee29a
Eric <eric.ledonge@gmail.com>
2020-09-20 12:50:29 -0400

    :white_check_mark: Unit tests Improvement

--- src/components/results/results-container/ResultsContainer.spec.js
+++ src/components/results/results-container/ResultsContainer.spec.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { mount } from 'enzyme';
 import ResultsContainer from './ResultsContainer';
 import appContext from '../../../context/appContext';
+import { findByTestAttr } from '../../../../test/testUtils';
 
-let mockAppContext;
+let state = {};
 
-const setup = () => {
+const setup = ({ areResultsReady }) => {
+  state.areResultsReady = areResultsReady || false;
+  state.stockPrices = [{ date: '2020-09-20', price: 169 }];
+  state.mediaCount = [{ mediaType: 'Facebook', positivePost: 9, negativePost: 4, attractiveness: 5 }];
+
   return mount(
-    <appContext.AppProvider>
+    <appContext.AppProvider value={[state, jest.fn()]}>
       <ResultsContainer />
     </appContext.AppProvider>
   );
@@ -16,26 +21,26 @@ const setup = () => {
 describe('<ResultsContainer />', () => {
   let wrapper;
 
-  beforeEach(() => {
-    wrapper = setup();
-  });
-
   it('renders without error', () => {
+    wrapper = setup({});
     expect(wrapper.exists()).toBeTruthy();
   });
 
-  // it('contains the stock symbol', () => {
-  //   const stockSymbol = findByTestAttr(wrapper, 'stock-symbol-component');
-  //   expect(stockSymbol.exists()).toBeTruthy();
-  // });
-  //
-  // it('contains the stock recommendation', () => {
-  //   const stockRecommendation = findByTestAttr(wrapper, 'stock-recommendation-component');
-  //   expect(stockRecommendation.exists()).toBeTruthy();
-  // });
-  //
-  // it('contains the stock prices', () => {
-  //   const stockPrices = findByTestAttr(wrapper, 'stock-prices-component');
-  //   expect(stockPrices.exists()).toBeTruthy();
-  // });
+  it('contains the stock symbol', () => {
+    wrapper = setup({ areResultsReady: true });
+    const stockSymbol = findByTestAttr(wrapper, 'stock-symbol-component');
+    expect(stockSymbol.exists()).toBeTruthy();
+  });
+
+  it('contains the stock recommendation', () => {
+    wrapper = setup({ areResultsReady: true });
+    const stockRecommendation = findByTestAttr(wrapper, 'stock-recommendation-component');
+    expect(stockRecommendation.exists()).toBeTruthy();
+  });
+
+  it('contains the stock prices', () => {
+    wrapper = setup({ areResultsReady: true });
+    const stockPrices = findByTestAttr(wrapper, 'stock-prices-component');
+    expect(stockPrices.exists()).toBeTruthy();
+  });
 });
